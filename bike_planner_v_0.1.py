dbb1bd45acbbdf68b890e9cbf961c08c16de9a83
Mads Høgenhaug <madshogenhaug3@gmail.com>
2025-03-11 18:38:45 +0100

    fix

--- bike_planner_v_0.1.py
+++ bike_planner_v_0.1.py
@@ -72,4 +72,8 @@ def route():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
+
+#For docker
+# if __name__ == '__main__':
+    # app.run(debug=True, host='0.0.0.0', port=5000)
